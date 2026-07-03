2e30922e02e939104f0e16f08b711cfdeb282537
Ihsan <apna_github_vzada6353@gmail.com>
2026-07-03 23:42:24 +0500

    fix TypeScript global mongoose type error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,9 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Please add MONGODB_URI in .env");
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+declare global {
+  var mongoose: {
+    conn: typeof import('mongoose') | null;
+    promise: Promise<typeof import('mongoose')> | null;
+  } | undefined;
 }
 
 let cached = global.mongoose;
@@ -12,15 +21,29 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+async function connectDB() {
+  if (cached!.conn) {
+    return cached!.conn;
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  if (!cached!.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached!.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached!.conn = await cached!.promise;
+  } catch (e) {
+    cached!.promise = null;
+    throw e;
+  }
+
+  return cached!.conn;
 }
 
-export default dbConnect;
+export default connectDB;
