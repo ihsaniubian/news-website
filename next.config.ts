d955d9b0e7cc1705d8483f31feec48921e958709
Ihsan <apna_github_vzada6353@gmail.com>
2026-07-08 22:26:00 +0500

    Updated config for cloudinary images

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'res.cloudinary.com', // Ab yahan cloudinary ka domain aayega
       },
     ],
   },
